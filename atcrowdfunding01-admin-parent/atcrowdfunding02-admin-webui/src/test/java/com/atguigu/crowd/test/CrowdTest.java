7ea2d689537e4ac517655250163b595183a23677
朱宝华 <897332413@qq.com>
2021-04-28 10:08:22 +0800

    增加logback.xml全局日志配置，以及测试的代码

--- atcrowdfunding01-admin-parent/atcrowdfunding02-admin-webui/src/test/java/com/atguigu/crowd/test/CrowdTest.java
+++ atcrowdfunding01-admin-parent/atcrowdfunding02-admin-webui/src/test/java/com/atguigu/crowd/test/CrowdTest.java
@@ -7,6 +7,8 @@ import javax.sql.DataSource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -21,6 +23,31 @@ public class CrowdTest {
 	private DataSource dataSource;
 	@Autowired
 	private AdminMapper adminMapper;
+	
+	
+	@Test
+	public void testLog() {
+		
+		// 1.获取Logger对象，这里传入的Class对象就是当前打印日志的类
+		Logger logger = LoggerFactory.getLogger(CrowdTest.class);
+		
+		// 2.根据不同日志级别打印日志
+		logger.debug("Hello I am Debug level!!!");
+		logger.debug("Hello I am Debug level!!!");
+		logger.debug("Hello I am Debug level!!!");
+		
+		logger.info("Info level!!!");
+		logger.info("Info level!!!");
+		logger.info("Info level!!!");
+		
+		logger.warn("Warn level!!!");
+		logger.warn("Warn level!!!");
+		logger.warn("Warn level!!!");
+		
+		logger.error("Error level!!!");
+		logger.error("Error level!!!");
+		logger.error("Error level!!!");
+	}
 	
 	@Test
 	public void testInsertAdmin() {
